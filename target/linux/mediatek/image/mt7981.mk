31ae00df211fe666d5916aba68d6a50ddb3ab081
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2025-03-11 01:41:18 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -629,24 +629,6 @@ define Device/cmcc_rax3000m-256m
 endef
 TARGET_DEVICES += cmcc_rax3000m-256m
 
-define Device/cmcc_xr30
-  DEVICE_VENDOR := CMCC
-  DEVICE_MODEL := XR30 NAND
-  DEVICE_DTS := mt7981-cmcc-xr30
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-samba4
-  SUPPORTED_DEVICES := cmcc,xr30
-  UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  IMAGE_SIZE := 116736k
-  KERNEL_IN_UBI := 1
-  IMAGES += factory.bin
-  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += cmcc_xr30
-
 define Device/cmcc_xr30-256m
   DEVICE_VENDOR := CMCC
   DEVICE_MODEL := XR30 NAND 256m
@@ -664,6 +646,24 @@ define Device/cmcc_xr30-256m
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += cmcc_xr30-256m
+
+define Device/cmcc_xr30
+  DEVICE_VENDOR := CMCC
+  DEVICE_MODEL := XR30 NAND
+  DEVICE_DTS := mt7981-cmcc-xr30
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-samba4
+  SUPPORTED_DEVICES := cmcc,xr30
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 116736k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += cmcc_xr30
 
 define Device/cmcc_xr30-emmc
   DEVICE_VENDOR := CMCC
